1180c14388e61121546783eb7d74c2e65a6fd813
w9mustafa-arch <w9mustafa@gmail.com>
2025-12-22 00:25:49 +0100

    Update navigation.ts

--- src/navigation.ts
+++ src/navigation.ts
@@ -1,5 +1,8 @@
 import { getPermalink, getBlogPermalink, getAsset } from './utils/permalinks';
 
+export const headerData = {
+  
+};
 
 export const footerData = {
   links: [
@@ -56,6 +59,7 @@ export const footerData = {
     { ariaLabel: 'X', icon: 'tabler:brand-x', href: '#' },
     { ariaLabel: 'Instagram', icon: 'tabler:brand-instagram', href: '#' },
     { ariaLabel: 'Facebook', icon: 'tabler:brand-facebook', href: '#' },
+    { ariaLabel: 'RSS', icon: 'tabler:rss', href: getAsset('/rss.xml') },
     { ariaLabel: 'Github', icon: 'tabler:brand-github', href: 'https://github.com/arthelokyo/astrowind' },
   ],
   footNote: `
